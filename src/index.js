323200cc81d665e6d49b78d158fd500808c93721
unknown <abtabilashbiju@gmail.com>
2022-08-20 12:15:04 +0530

    Deploy my app to git hub pages

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
     <div>
-        <BrowserRouter>
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </div>,
     document.getElementById("root")
 )
