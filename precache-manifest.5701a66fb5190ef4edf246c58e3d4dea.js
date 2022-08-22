148ba02e5a7bbdd501606bedd98fcee73e0387d9
unknown <abtabilashbiju@gmail.com>
2022-08-22 12:09:55 +0530

    Updates

---
+++ precache-manifest.5701a66fb5190ef4edf246c58e3d4dea.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "84362f7980154eadc3a5",
+    "url": "/clone-app/static/css/main.2fa037c6.chunk.css"
+  },
+  {
+    "revision": "84362f7980154eadc3a5",
+    "url": "/clone-app/static/js/main.26bfbce4.chunk.js"
+  },
+  {
+    "revision": "53d2228978f00e6fc1bf",
+    "url": "/clone-app/static/js/runtime~main.3dac79af.js"
+  },
+  {
+    "revision": "e1e22f851754f409fc04",
+    "url": "/clone-app/static/js/2.e278f374.chunk.js"
+  },
+  {
+    "revision": "246a202e1bf528c5c2141c280ed55661",
+    "url": "/clone-app/static/media/avatar.246a202e.png"
+  },
+  {
+    "revision": "b32933a6e23824c7c5824cd42cd87439",
+    "url": "/clone-app/static/media/Banner.b32933a6.jpg"
+  },
+  {
+    "revision": "f800889d5c5d2b29f6a70af6a8c52e5c",
+    "url": "/clone-app/static/media/video-tv.f800889d.gif"
+  },
+  {
+    "revision": "77994a67327ba957dfd880e33a91f041",
+    "url": "/clone-app/static/media/tv.77994a67.png"
+  },
+  {
+    "revision": "1de1ead6cbab413adaab0b7bb6521fa9",
+    "url": "/clone-app/static/media/mobile.1de1ead6.jpg"
+  },
+  {
+    "revision": "583558aa343aafa29e2aa7daec861023",
+    "url": "/clone-app/static/media/video-computer.583558aa.gif"
+  },
+  {
+    "revision": "91ac8021bd945d74b05cda468541241a",
+    "url": "/clone-app/static/media/computer.91ac8021.png"
+  },
+  {
+    "revision": "d9e8458b0d8d90199b950b7a67234f5b",
+    "url": "/clone-app/static/media/children.d9e8458b.jpg"
+  },
+  {
+    "revision": "76a8d35390d941faab0b760a30971b5d",
+    "url": "/clone-app/static/media/OTT.76a8d353.mp4"
+  },
+  {
+    "revision": "8de03284e3bda4b3eb53566429018342",
+    "url": "/clone-app/index.html"
+  }
+];
